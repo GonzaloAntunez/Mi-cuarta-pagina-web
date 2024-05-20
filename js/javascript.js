50ce7266c8e8aaafb6eb5f08a117546b78646344
GonzaloAntunez <gonzaantuneez@gmail.com>
2024-05-20 20:34:00 +0200

    Contenido del proyecto
    Se ha añadido a la carpeta todos los archivos que forman el proyecto web

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+let facebook = document.getElementById("facebook");
+facebook.addEventListener("mouseover", (e) =>{
+    e.target.src = "./assets/imgs/footer/icons8-facebook-nuevo-48.png"
+});
+facebook.addEventListener("mouseout", (e) =>{
+    e.target.src = "./assets/imgs/footer/icons8-facebook-nuevo-50.png"
+});
+
+let instagram = document.getElementById("instagram");
+instagram.addEventListener("mouseover", (e) =>{
+    e.target.src = "./assets/imgs/footer/icons8-instagram-48.png"
+});
+instagram.addEventListener("mouseout", (e) =>{
+    e.target.src = "./assets/imgs/footer/icons8-instagram-50.png"
+});
+
+let tiktok = document.getElementById("tiktok");
+tiktok.addEventListener("mouseover", (e) =>{
+    e.target.src = "./assets/imgs/footer/icons8-tik-tok-48.png"
+});
+tiktok.addEventListener("mouseout", (e) =>{
+    e.target.src = "./assets/imgs/footer/icons8-tik-tok-50.png"
+});
